95f76d5321d408216e4c413d39358ded153ac0b5
rmflight <r.flight79@gmail.com>
2012-12-24 22:35:44 -0500

    input roxygen2 type documentation

--- R/blogr_functions.R
+++ R/blogr_functions.R
@@ -1,11 +1,9 @@
-# function to take a knitr RHTML document and create
-require(knitr)
-require(markdown)
-
-knit_template <- function(input_Rmd, all_options){
-  knit(input_Rmd)
-}
-
+#' Create the header of the page
+#' 
+#' This function takes the full set of blog options, and uses that to create the header
+#' that will be displayed on the main index page and each blog post page
+#' 
+#' @param all_options The full set of options. Will generally be .all_options
 createHeader <- function(all_options){
   head1 <- ""
   if (!(is.na(all_options["title"]))){
@@ -17,11 +15,25 @@ createHeader <- function(all_options){
   return(head1)
 }
 
+
+#' Create the index page of recent blog posts
+#' 
+#' Takes all_options and creates the index page of recent posts. The number of posts, and many
+#' other options should be set in the file _blog_options.R. This file will be sourced, and the
+#' options used in subsequent creation of the main index and individual posts.
 createMain <- function(all_options){
   
 }
 
-createPost <- function(all_options, filename){
+
+#' Create an individual post
+#' 
+#' Takes a supplied file \code{filename} and generates an html page based on a given template
+#' 
+#' @param all_options the options for the blog generally
+#' @param filename the MD file to be processed to generate the individual post
+#' @export
+createPost <- function(filename, all_options){
 	.post_options <- NA # set this to start
 	
 	tmpKnit <- knit(filename, "tmp.md")
@@ -35,6 +47,14 @@ addAuthor <- function(inAuthor){
   return(inAuthor)
 }
 
+
+#' Set knitr hooks
+#' 
+#' Set chunk hooks that will be used in subsequent code chunks. Because \code{\link{knitr}} 
+#' requires custom hooks to be set for a document, this function should be called first
+#' in a given MD document, and then the post settings defined in the next code chunk.
+#'
+#' @export
 setHooks <- function(){
 	knit_hooks$set(post.options = function(before, options, envir){
 		if (!before){
